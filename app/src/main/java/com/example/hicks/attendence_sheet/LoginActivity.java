49ebef38f4dfc47808ff345696ecb67080d04e24
gildarts99 <hicks.matthew99@hotmail.com>
2018-11-05 02:42:02 -0600

    hello

--- app/src/main/java/com/example/hicks/attendence_sheet/LoginActivity.java
+++ app/src/main/java/com/example/hicks/attendence_sheet/LoginActivity.java
@@ -1,7 +1,13 @@
 package com.example.hicks.attendence_sheet;
-
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+//import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +15,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+
+        final Button bLogin = (Button) findViewById(R.id.bLogin);
+
+        final TextView tvRegister = (TextView) findViewById(R.id.tvRegister);
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+
+            }
+        });
     }
 }
